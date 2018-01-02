aa886ad0f799e4db22a96f07faa521e769aff1a6
Jiachen <Jiachen1018@outlook.com>
2018-01-02 17:46:12 +0000

    CSS

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # The following part is used for practice.
 from mysite.views import hello, my_homepage_view, current_datetime, hours_ahead
 # this part is our app
 from mysite.views import create_new_group, Homepage,Forgot, group_detail, group_list,Login, sign_up
+import settings
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
@@ -20,4 +22,6 @@ urlpatterns = patterns('',
     ('^Login/$', Login),
     ('^sign_up/$', sign_up),
 )
+
+urlpatterns += staticfiles_urlpatterns()
 
