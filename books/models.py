5d6f0b84d726ec242ac680493b87e56a7f5e1c9d
Vicky9571 <929394485@qq.com>
2018-01-04 17:24:37 +0000

    database
    database

--- books/models.py
+++ books/models.py
@@ -1,66 +1,46 @@
 from django.db import models
 
-'''
-class Publisher(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
-
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.EmailField()
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-'''
-class User(models.Model):
-    phone = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=128)
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
+    email = models.EmailField(blank=False,default='DEFAULT VALUE')
+    username = models.CharField(max_length=30,default='DEFAULT VALUE')
+    password = models.CharField(max_length=128,default='DEFAULT VALUE')
+    firstname = models.CharField(max_length=30,default='DEFAULT VALUE')
+    lastname = models.CharField(max_length=30,default='DEFAULT VALUE')
 
 
 class Country(models.Model):
-    c_name = models.CharField(max_length=30)
+    c_name = models.CharField(max_length=30,default='DEFAULT VALUE')
+    c_url_photo = models.URLField(default='DEFAULT VALUE')
 
 
 class City(models.Model):
-    ct_name = models.CharField(max_length=30)
+    ct_name = models.CharField(max_length=30,default='DEFAULT VALUE')
     ct_country = models.ForeignKey(Country, related_name='City_Country')
 
 
 class Group(models.Model):
-    g_name = models.CharField(max_length=30)
+    g_name = models.CharField(max_length=30,default='DEFAULT VALUE')
     g_organiser = models.ForeignKey(User, related_name='user_organiser')
-    g_member = models.CharField(max_length=100,blank=False)
+    g_member = models.CharField(blank=False,default='DEFAULT VALUE')
     g_place = models.ForeignKey(City, related_name='group_gatherplace')
     g_city = models.ForeignKey(City, related_name='group_city')
-    g_content = models.TextField(max_length=100)
-    g_state = models.IntegerField('In a team or completed')
+    g_content = models.TextField(max_length=100,default='DEFAULT VALUE')
+    g_state = models.IntegerField('In a team or completed',default='DEFAULT VALUE')
     g_country = models.ForeignKey(Country, related_name='group_country')
+    g_createdtime = models.DateTimeField(default='DEFAULT VALUE')
 
 
 class Comment(models.Model):
     article = models.ForeignKey(Group, related_name='article_comment')
-    detail = models.TextField(max_length=128)
+    detail = models.TextField(max_length=128,default='DEFAULT VALUE')
     user = models.ForeignKey(User, related_name='user_comment')
+    created_time = models.DateTimeField(default='DEFAULT VALUE')
 
 
 class GroupMember(models.Model):
     gm_member = models.ManyToManyField(User, related_name='member_group')
     gm_group = models.ForeignKey(Group, related_name='user_group')
+
+
+
