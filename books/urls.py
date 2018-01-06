36369f91c30e67d991f0d0942f2afb23ddafecb7
yw3y17 <yw3y17@soton.ac.uk>
2018-01-07 05:09:17 +0800

    add books/

--- books/urls.py
+++ books/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^Login/$', views.login, name='Login'),
     url(r'^regist/$', views.regist, name='regist'),
     url(r'^index/$', views.index, name='index'),
     url(r'^logout/$', views.logout, name='logout'),
     url('^create_new_group/$', views.create_new_group),
     url('^homepage/$', views.homepage),
     url('^group_detail/$', views.group_detail),
-    url('^grouplist/$', views.grouplist),
-    url('^my_group/$', views.my_group),
-    url('^group_detail_organiser/$', views.group_detail_organiser),
+    url('^groupList/$', views.grouplist),
+    url('^myGroups/$', views.my_group),
+    url('^myGroupDetail_organiser/$', views.group_detail_organiser),
     url('^myGroupDetail_member/$', views.group_detail_member),
     url('^regist/$', views.regist),
 ]
