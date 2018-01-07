5841588dc1ea296ee0b8e1fe603c3185d64236b5
Setiawan Utama <setiawanutama@gmail.com>
2018-01-07 18:10:27 +0000

    Updated settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,6 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/templates/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'templates'),
